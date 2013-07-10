3ca10e379b3fb13240ef98b9018a50fe6639bcea
Сергей Швецов <lollipop@Mac-mini-Sergej.local>
2013-07-10 18:42:01 +0400

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hochuli::Application.config.secret_token = '46eb2027a53a1eabbd6caf91e78b57cb7003b7a7b7e8c9a1e18b819e633833c657dab05dba563121485a91d610e2c10fb157e685b5971fbd13bbaad2a230a458'
