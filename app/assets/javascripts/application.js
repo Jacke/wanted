80fd7c0bbdee9394f34b6b367b014154e538a605
Shvetsov <sergej.shvecov@gmail.com>
2013-07-11 15:03:33 +0400

    css left box change on resize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,27 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(window).resize(function window_resize(){
+  var page_w = $("html").width();
+  var box = $('#left_group'); // float-fixed block
+
+  if(page_w < 935) {
+      box.css('position', 'absolute');
+      box.css('top', 0);
+  } else {
+      box.css('position', 'fixed');
+      box.css('top', 85);
+  }
+});
+
+/*$(function() {
+    var box = $('#left_groupp'); // float-fixed block
+
+    var top = box.offset().top - parseFloat(box.css('marginTop').replace(/auto/, 0));
+    $(window).scroll(function(){
+        var windowpos = $(window).scrollTop();
+            box.css('position', 'fixed');
+            box.css('top', '85px');
+    });
+});*/
