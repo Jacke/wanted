9ce21600a273721806885320cfdd8f0404cf1a84
Сергей Швецов <sergej.shvecov@gmail.com>
2013-07-11 23:56:44 +0400

    add right column

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,18 +14,6 @@
 //= require jquery_ujs
 //= require_tree .
 
-$(window).resize(function window_resize(){
-  var page_w = $("html").width();
-  var box = $('#left_group'); // float-fixed block
-
-  if(page_w < 935) {
-      box.css('position', 'absolute');
-      box.css('top', 0);
-  } else {
-      box.css('position', 'fixed');
-      box.css('top', 85);
-  }
-});
 
 /*$(function() {
     var box = $('#left_groupp'); // float-fixed block
