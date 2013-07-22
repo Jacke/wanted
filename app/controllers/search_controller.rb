fb25b751b7fa8128e035417c4b00080e1b7bf6d9
Shvetsov <sergej.shvecov@gmail.com>
2013-07-22 10:29:36 +0400

    показать проблему

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -19,6 +19,22 @@ class SearchController < ApplicationController
     #end
   end
 
+  def save_site
+    @site = clean_url(params[:site][:url])
+    full_url = 'href="http://www.'+@site+'/'
+    require 'open-uri'
+    @page = open('http://www.'+@site).read
+    @page.gsub!('href="/',full_url)
+    full_url = 'src="http://www.'+@site+'/'
+    @page.gsub!('src="/',full_url)
+    @page.gsub!('target="_blank"','')
+    @page.gsub!('<a href="http://www.','<a href="/go/site?'+'site%5Burl%5D='+@site+'&site%5Bfull_url%5D=')
+    @page = @page.dup.force_encoding('UTF-8')#sthash.JAbhtVv9.dpuf
+    File.open(Rails.root+'public/fraim.html', 'w') do |file|
+     file << @page
+    end
+  end
+
   def redirect
     @site = clean_url(params[:site][:url])
     @full_site = clean_url(params[:site][:full_url])
