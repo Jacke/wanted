147845ad8e1d684e7b72cd5d277b982095493199
Stan <iamjacke@gmail.com>
2013-08-28 02:57:47 +0400

    Trying sphinx

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 require 'bundler/capistrano'
+require 'thinking_sphinx/deploy/capistrano'
 
 ## Чтобы не хранить database.yml в системе контроля версий, поместите
 ## dayabase.yml в shared-каталог проекта на сервере и раскомментируйте
@@ -66,6 +67,17 @@ set :repository,      "ssh://#{user}@#{deploy_server}/home/#{user}/git/#{applica
 # set :repository,    "ssh://Jacke@github.com/Jacke/wanted.git"
 
 ## --- Ниже этого места ничего менять скорее всего не нужно ---
+before 'deploy:update_code', 'thinking_sphinx:stop'
+after  'deploy:update_code', 'thinking_sphinx:start'
+
+namespace :sphinx do
+  desc "Symlink Sphinx indexes"
+  task :symlink_indexes, :roles => [:app] do
+    run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+  end
+end
+
+after 'deploy:finalize_update', 'sphinx:symlink_indexes'
 
 before 'deploy:finalize_update', 'set_current_release'
 task :set_current_release, :roles => :app do
@@ -75,7 +87,9 @@ end
   set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
 
 
+
 # - for unicorn - #
+
 namespace :deploy do
   desc "Start application"
   task :start, :roles => :app do
