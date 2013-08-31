ce0221632d5a1ad446aaf2da39c2447c5575d3b9
Stan <iamjacke@gmail.com>
2013-08-31 09:02:22 +0200

    otto fixed

--- app/controllers/concerns/shop_parser.rb
+++ app/controllers/concerns/shop_parser.rb
@@ -40,6 +40,7 @@ module ShopParser
     @page.gsub!('{"url": "/', '{"url": "http://'+host+'/')
     @page.gsub!('"url": "/', '"url": "http://'+host+'/')
     @page.gsub!('"category_url": "/', '"category_url": "http://'+host+'/')
+    @page.gsub!('data-producturl="/', 'data-producturl="http://'+host+'/')
     @page.gsub!('<a class="b-multi-top__item-link" url="/', '<a class="b-multi-top__item-link url="http://'+host+'/')
     @page.gsub!('<a class="b-multi__item-link" url="/','<a class="b-multi__item-link" url="http://'+host+'/')
     @page.gsub!('data-link="/','data-link="http://'+host+'/')
