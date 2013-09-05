a33713f23bfb5eeaef0721cc430971a896ac05ad
Stan <iamjacke@gmail.com>
2013-09-05 18:35:09 +0200

    avatar bug

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Hochuli::Application.routes.draw do
                                     sessions: 'sessions'
                                   }
   devise_scope :user do
+    put '/registrations/update_avatar' => "registrations#update_avatar", :as => :update_avatar 
     get '/registrations/update_avatar' => "registrations#update_avatar", :as => :update_avatar  
     get '/confirm/:confirmation_token', :to => "devise/confirmations#show", :as => "user_confirm", :only_path => false
   end                  
